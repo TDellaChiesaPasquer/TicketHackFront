23635176ce7e12f84a67dd7ce24dd632f1ed933c
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-01 16:28:02 +0200

    ajout script login

---
+++ signup.js
@@ -0,0 +1,23 @@
+document.querySelector('#btn-signup', function (event) {
+    const firstName = document.querySelector('#firstName');
+    const lastName = document.querySelector('#lastName');
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const confirmPassword = document.querySelector('#confirmPassword').value;
+    if (password !== confirmPassword) {
+        return;
+    }
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: {firstName, lastName, email, password}
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (!data.result) {
+            return;
+        }
+        window.location.href = './index.html';
+    })
+    .catch(error => console.log(error));
+})
