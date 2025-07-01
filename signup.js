ae9ced7e124a53909921e37d258e6ee95071b118
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-01 16:45:32 +0200

    ajout sign up

--- signup.js
+++ signup.js
@@ -5,6 +5,7 @@ document.querySelector('#btn-signup').addEventListener('click', function (event)
     const password = document.querySelector('#password').value;
     const confirmPassword = document.querySelector('#confirmPassword').value;
     if (password !== confirmPassword) {
+        document.querySelector('#error').innerHTML = 'The passwords are not the same.';
         return;
     }
     fetch('http://localhost:3000/users/signup', {
@@ -15,9 +16,13 @@ document.querySelector('#btn-signup').addEventListener('click', function (event)
     .then(response => response.json())
     .then(data => {
         if (!data.result) {
+            document.querySelector('#error').innerHTML = data.error;
             return;
         }
         window.location.href = './index.html';
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+        console.log(error);
+        document.querySelector('#error').innerHTML = data.error;
+    });
 })
